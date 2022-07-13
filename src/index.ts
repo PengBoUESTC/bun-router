2f51f72c0abd69c4cae35487e5cb0dc2097fd0e3
pengbo43 <pengbo43@jd.com>
2022-07-13 11:15:09 +0800

    feat: init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('test')
+export type Handler = (req: Request) => Response
+
+export class BunRouter {
+  public routerMap: Record<string, Handler>
+  constructor(routerMap) {
+    this.routerMap = routerMap
+  }
+
+  dispatch(api: string, req: Request): Response {
+    return this.routerMap[api](req)
+  }
+}
